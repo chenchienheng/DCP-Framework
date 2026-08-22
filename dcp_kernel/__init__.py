1b2ea9607d1bec6ccdb83e17bf6309c85cb94b7f
陳千恆 <vista0329@gmail.com>
2026-08-22 13:52:32 +0800

    dcp: expose coexistence translation primitive

--- dcp_kernel/__init__.py
+++ dcp_kernel/__init__.py
@@ -23,184 +23,41 @@ from .models import (
     TransitionEvaluation,
     TriRootState,
 )
-from .action_gate import (
-    ActionGateAssessment,
-    ActionGateInput,
-    EffectClass,
-    RiskLevel,
-    assess_action_gate,
-)
-from .carrier_binding import (
-    CarrierCandidate,
-    CarrierClass,
-    CarrierNeed,
-    CarrierResolution,
-    resolve_carrier_binding,
-    validate_carrier_substitution,
-)
-from .consequence import (
-    ActionResponsibilityContract,
-    ConsequenceAssessment,
-    ConsequenceInput,
-    compile_action_responsibility,
-    derive_next_condition,
-)
-from .evidence import (
-    EvidenceMode,
-    OwnerExitAssessment,
-    OwnerExitEvidence,
-    assess_owner_exit_evidence,
-)
-from .family_metabolism import (
-    FamilyMetabolismAssessment,
-    FamilyMetabolismInput,
-    FamilyMetabolismState,
-    assess_family_metabolism,
-)
+from .action_gate import ActionGateAssessment, ActionGateInput, EffectClass, RiskLevel, assess_action_gate
+from .carrier_binding import CarrierCandidate, CarrierClass, CarrierNeed, CarrierResolution, resolve_carrier_binding, validate_carrier_substitution
+from .coexistence import CompatibilityState, CoexistenceAssessment, CoexistenceInput, NativeModel, assess_coexistence
+from .consequence import ActionResponsibilityContract, ConsequenceAssessment, ConsequenceInput, compile_action_responsibility, derive_next_condition
+from .evidence import EvidenceMode, OwnerExitAssessment, OwnerExitEvidence, assess_owner_exit_evidence
+from .family_metabolism import FamilyMetabolismAssessment, FamilyMetabolismInput, FamilyMetabolismState, assess_family_metabolism
 from .learning import assess_learning_input
-from .meaning_compile import (
-    MeaningCompileAssessment,
-    MeaningCompileInput,
-    MeaningLevel,
-    compile_meaning,
-)
-from .platform import (
-    PlatformLoopResult,
-    PlatformPlan,
-    WorkContract,
-    build_reentry_state,
-    compile_work_contract,
-    complete_fixture_loop,
-)
-from .reader_policy import (
-    ReaderAssessment,
-    ReaderDisposition,
-    ReaderRequest,
-    assess_reader_request,
-)
-from .reference_census import (
-    ReferenceClass,
-    ReferenceObservation,
-    classify_reference,
-    has_proven_live_caller,
-    has_unknown_hold,
-    scan_text_map,
-)
-from .resolution import (
-    compute_affected_cone,
-    evaluate_claim_ceiling,
-    resolve_capability_binding,
-    resolve_current,
-)
-from .retirement import (
-    RetirementAssessment,
-    RetirementInput,
-    RetirementState,
-    assess_retirement,
-)
+from .meaning_compile import MeaningCompileAssessment, MeaningCompileInput, MeaningLevel, compile_meaning
+from .platform import PlatformLoopResult, PlatformPlan, WorkContract, build_reentry_state, compile_work_contract, complete_fixture_loop
+from .reader_policy import ReaderAssessment, ReaderDisposition, ReaderRequest, assess_reader_request
+from .reference_census import ReferenceClass, ReferenceObservation, classify_reference, has_proven_live_caller, has_unknown_hold, scan_text_map
+from .resolution import compute_affected_cone, evaluate_claim_ceiling, resolve_capability_binding, resolve_current
+from .retirement import RetirementAssessment, RetirementInput, RetirementState, assess_retirement
 from .return_state import IllegalReturnTransition, ReturnClosure
-from .schedule_effect import (
-    ScheduleEffectAssessment,
-    ScheduleEffectInput,
-    ScheduleEffectState,
-    TriggerClass,
-    assess_schedule_effect,
-)
-from .successor import (
-    CoverageState,
-    SuccessorCoverageAssessment,
-    SuccessorCoverageInput,
-    assess_successor_coverage,
-)
+from .schedule_effect import ScheduleEffectAssessment, ScheduleEffectInput, ScheduleEffectState, TriggerClass, assess_schedule_effect
+from .successor import CoverageState, SuccessorCoverageAssessment, SuccessorCoverageInput, assess_successor_coverage
 from .transition import evaluate_transition
 
 __all__ = [
-    "ActionGateAssessment",
-    "ActionGateInput",
-    "ActionResponsibilityContract",
-    "AffectedCone",
-    "CapabilityBinding",
-    "CapabilityResolution",
-    "CarrierCandidate",
-    "CarrierClass",
-    "CarrierNeed",
-    "CarrierResolution",
-    "ClaimCeiling",
-    "ClaimEvidence",
-    "ConsequenceAssessment",
-    "ConsequenceInput",
-    "CoverageState",
-    "CurrentCandidate",
-    "CurrentResolution",
-    "CurrentResolutionStatus",
-    "Decision",
-    "EffectClass",
-    "EvidenceMode",
-    "FamilyMetabolismAssessment",
-    "FamilyMetabolismInput",
-    "FamilyMetabolismState",
-    "IllegalReturnTransition",
-    "InvariantCore",
-    "LifecycleState",
-    "LearningAssessment",
-    "LearningDisposition",
-    "LearningInput",
-    "MeaningCompileAssessment",
-    "MeaningCompileInput",
-    "MeaningLevel",
-    "Motion",
-    "MotionObservation",
-    "Need",
-    "OwnerExitAssessment",
-    "OwnerExitEvidence",
-    "PlatformLoopResult",
-    "PlatformPlan",
-    "ReaderAssessment",
-    "ReaderDisposition",
-    "ReaderRequest",
-    "ReferenceClass",
-    "ReferenceObservation",
-    "ReentryState",
-    "RetirementAssessment",
-    "RetirementInput",
-    "RetirementState",
-    "ReturnClosure",
-    "ReturnState",
-    "RiskLevel",
-    "ScheduleEffectAssessment",
-    "ScheduleEffectInput",
-    "ScheduleEffectState",
-    "StableLife",
-    "SuccessorCoverageAssessment",
-    "SuccessorCoverageInput",
-    "Transition",
-    "TransitionEvaluation",
-    "TriRootState",
-    "TriggerClass",
-    "WorkContract",
-    "assess_action_gate",
-    "assess_family_metabolism",
-    "assess_learning_input",
-    "assess_owner_exit_evidence",
-    "assess_reader_request",
-    "assess_retirement",
-    "assess_schedule_effect",
-    "assess_successor_coverage",
-    "build_reentry_state",
-    "classify_reference",
-    "compile_action_responsibility",
-    "compile_meaning",
-    "compile_work_contract",
-    "complete_fixture_loop",
-    "compute_affected_cone",
-    "derive_next_condition",
-    "evaluate_claim_ceiling",
-    "evaluate_transition",
-    "has_proven_live_caller",
-    "has_unknown_hold",
-    "resolve_capability_binding",
-    "resolve_carrier_binding",
-    "resolve_current",
-    "scan_text_map",
-    "validate_carrier_substitution",
+    "ActionGateAssessment", "ActionGateInput", "ActionResponsibilityContract", "AffectedCone",
+    "CapabilityBinding", "CapabilityResolution", "CarrierCandidate", "CarrierClass", "CarrierNeed", "CarrierResolution",
+    "ClaimCeiling", "ClaimEvidence", "CompatibilityState", "CoexistenceAssessment", "CoexistenceInput",
+    "ConsequenceAssessment", "ConsequenceInput", "CoverageState", "CurrentCandidate", "CurrentResolution",
+    "CurrentResolutionStatus", "Decision", "EffectClass", "EvidenceMode", "FamilyMetabolismAssessment",
+    "FamilyMetabolismInput", "FamilyMetabolismState", "IllegalReturnTransition", "InvariantCore", "LifecycleState",
+    "LearningAssessment", "LearningDisposition", "LearningInput", "MeaningCompileAssessment", "MeaningCompileInput",
+    "MeaningLevel", "Motion", "MotionObservation", "NativeModel", "Need", "OwnerExitAssessment", "OwnerExitEvidence",
+    "PlatformLoopResult", "PlatformPlan", "ReaderAssessment", "ReaderDisposition", "ReaderRequest", "ReferenceClass",
+    "ReferenceObservation", "ReentryState", "RetirementAssessment", "RetirementInput", "RetirementState", "ReturnClosure",
+    "ReturnState", "RiskLevel", "ScheduleEffectAssessment", "ScheduleEffectInput", "ScheduleEffectState", "StableLife",
+    "SuccessorCoverageAssessment", "SuccessorCoverageInput", "Transition", "TransitionEvaluation", "TriRootState", "TriggerClass",
+    "WorkContract", "assess_action_gate", "assess_coexistence", "assess_family_metabolism", "assess_learning_input",
+    "assess_owner_exit_evidence", "assess_reader_request", "assess_retirement", "assess_schedule_effect",
+    "assess_successor_coverage", "build_reentry_state", "classify_reference", "compile_action_responsibility",
+    "compile_meaning", "compile_work_contract", "complete_fixture_loop", "compute_affected_cone", "derive_next_condition",
+    "evaluate_claim_ceiling", "evaluate_transition", "has_proven_live_caller", "has_unknown_hold",
+    "resolve_capability_binding", "resolve_carrier_binding", "resolve_current", "scan_text_map", "validate_carrier_substitution",
 ]
